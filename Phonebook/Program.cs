f7a37acd192448f3e9f15b2a90d3250296e96cad
Jc9697 <124229632+Jc9697@users.noreply.github.com>
2026-01-12 13:41:10 -0600

    Added defaulttokenproviders method

--- Phonebook/Program.cs
+++ Phonebook/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Phonebook.Data;
 using Microsoft.AspNetCore.Identity;
+using Phonebook.Services;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,11 +12,14 @@ builder.Services.AddDbContext<PhonebookContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<PhonebookContext>();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<PhonebookContext>()
+    .AddDefaultTokenProviders();
 builder.Services.AddRazorPages(options =>
 {
     options.Conventions.AddPageRoute("/Identity/Account/Register", "");
 });
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
---
+++ Phonebook/Services/EmailSender.cs
@@ -0,0 +1,37 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
+
+namespace Phonebook.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task SendEmailAsync(string email, string subject, string messageBody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Jerry Chavis", "eleanore.veum18@ethereal.email"));
+            message.To.Add(new MailboxAddress("Jerry Chavis", "jerrychavis97@gmail.com"));
+            message.Subject = subject;
+            message.Body = new TextPart("plain") { Text = messageBody };
+
+            using (var client = new SmtpClient())
+                try
+                {
+                    {
+                        await client.ConnectAsync("smtp.ethereal.com", 587, SecureSocketOptions.StartTls);
+                        await client.AuthenticateAsync("eleanore.veum18@ethereal.email", "umSKtvhvCeSFHGUr2s");
+                        await client.SendAsync(message);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error sending email" + ex.ToString());
+                }
+                finally
+                {
+                    await client.DisconnectAsync(true);
+                }
+        }
+    }
+}
